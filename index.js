bfb4079abf9d5708b90583f455b23ce4a250f267
Fernando <fb@gamekaiju.monster>
2022-08-13 18:41:55 +0100

    Ready to release.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
-import url from 'url'
-import path from 'path'
-import sortObject from 'sort-object'
+import { readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
 
-const __filename = url.fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const packageJsonPath = path.join(__dirname, 'package.json')
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
-const sortedPackageJson = sortObject(packageJson)
-fs.writeFileSync(packageJsonPath, JSON.stringify(sortedPackageJson, null, 2))
+const packageJson = JSON.parse(
+  readFileSync(join(process.cwd(), 'package.json'), 'utf8'),
+)
 
-console.log('File package.json has been sorted.')
+const sortedPackageJson = JSON.stringify(
+  Object.keys(packageJson)
+    .sort()
+    .reduce((acc, key) => {
+      acc[key] = packageJson[key]
+      return acc
+    }, {}),
+  null,
+  2,
+)
+
+writeFileSync(join(process.cwd(), 'package.json'), sortedPackageJson)
+
+console.log('Sorted package.json')
